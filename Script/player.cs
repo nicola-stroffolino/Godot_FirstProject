062795ad70bc100a8b573c3bb38b53725d866257
HotBaconWasTaken <91049774+nicola-stroffolino@users.noreply.github.com>
2023-11-03 11:52:30 +0100

    Basic Jump Function

--- Script/player.cs
+++ Script/player.cs
@@ -27,7 +27,7 @@ public class player : KinematicBody {
 	private Vector3 Strafe = Vector3.Zero;
 	public override void _PhysicsProcess(float delta) {
 		float inertia = delta * 3;
-		if (Input.IsActionPressed("move_right") || Input.IsActionPressed("move_left") || Input.IsActionPressed("move_back") || Input.IsActionPressed("move_forward")){
+		if (Input.IsActionPressed("move_right") || Input.IsActionPressed("move_left") || Input.IsActionPressed("move_back") || Input.IsActionPressed("move_forward")) {
 			var HCamRotation = GetNode<Spatial>("CameraRoot/Horizontal").GlobalTransform.basis.GetEuler().y;
 			Vector3 Direction = new Vector3 {
 				x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"),
@@ -53,12 +53,67 @@ public class player : KinematicBody {
 			MovementStatus(-1f, delta);
 		}
 
-		GD.Print(GetNode<AnimationTree>("AnimationTree").Get("parameters/iwr_blend/blend_amount"));
-		MoveAndSlide(Velocity, Vector3.Up);
-	}
+		if (IsOnFloor() && Input.IsActionJustPressed("jump")) {
+			Velocity.y = JumpSpeed;
+		} else if (!IsOnFloor()) {
+			Velocity.y -= Gravity * delta;
+		} else {
+			Velocity.y = 0;
+		}
 
-	private void Jump() => Velocity.y = JumpSpeed;
-	private void SufferGravity(float delta) => Velocity.y -= Gravity * delta;
+		MoveAndSlide(Velocity, Vector3.Up);
+		// if (!IsOnFloor()) SufferGravity(delta);
+		// else if (Input.IsActionPressed("jump")) {
+		// 	GetNode<AnimationTree>("AnimationTree").Set("parameters/move_jump/active", new AnimationNodeOneShot());
+		// 	Jump();
+		// }
+
+		// if (Input.IsActionPressed("move_right") ||
+		// 	Input.IsActionPressed("move_left") ||
+		// 	Input.IsActionPressed("move_back") ||
+		// 	Input.IsActionPressed("move_forward")
+		// ){
+		// 	var HCamRotation = GetNode<Spatial>("CameraRoot/Horizontal").GlobalTransform.basis.GetEuler().y;
+
+		// 	Vector3 Direction = new Vector3 {
+		// 		x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"),
+		// 		y = 0,
+		// 		z = Input.GetActionStrength("move_back") - Input.GetActionStrength("move_forward")
+		// 	};
+
+		// 	StrafeDir = Direction;
+
+		// 	Direction = Direction.Rotated(Vector3.Up, HCamRotation).Normalized();
+
+		// 	Velocity = Velocity.LinearInterpolate(Direction * ActualSpeed, delta * 3);
+
+		// 	// Rotate Armature Facing Direction
+		// 	var Armature = GetNode<Spatial>("Armature");
+		// 	Armature.Rotation = new Vector3 {
+		// 		x = (float)Math.PI/2, // 90 deg = π/2
+		// 		y = Mathf.LerpAngle(Armature.Rotation.y, HCamRotation + (float)Math.PI, delta * AngularAcceleration)
+		// 	};
+
+		// 	if (Input.IsActionPressed("sprint")) {
+		// 		ActualSpeed = WalkingSpeed * RunningSpeedMultiplier;
+		// 		MovementStatus(1, delta);
+		// 	} else {
+		// 		ActualSpeed = WalkingSpeed;
+		// 		MovementStatus(0, delta);
+		// 	}
+		// } else {	
+		// 	Velocity.x = Mathf.Lerp(Velocity.x, 0f, delta * 3);
+		// 	Velocity.z = Mathf.Lerp(Velocity.z, 0f, delta * 3);
+		// 	StrafeDir = Vector3.Zero;
+		// 	MovementStatus(-1, delta);
+		// }
+
+		// Strafe = Strafe.LinearInterpolate(StrafeDir, delta * 3);
+
+		// GetNode<AnimationTree>("AnimationTree").Set("parameters/walk_blend/blend_position", new Vector2(-Strafe.x, -Strafe.z));
+		// GetNode<AnimationTree>("AnimationTree").Set("parameters/run_blend/blend_position", new Vector2(-Strafe.x, -Strafe.z));
+	}
+	
 	private void MovementStatus(float value, float delta) => GetNode<AnimationTree>("AnimationTree").Set(
 		"parameters/iwr_blend/blend_amount", Mathf.Lerp(
 			(float)GetNode<AnimationTree>("AnimationTree").Get("parameters/iwr_blend/blend_amount"), value, delta * 3
