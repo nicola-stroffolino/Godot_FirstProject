c057f109a9c74740d765b53853940e3e5518ca55
Nicola Stroffolino <91049774+nicola-stroffolino@users.noreply.github.com>
2023-11-20 19:53:08 +0100

    building system about 60% done

--- Script/Player.cs
+++ Script/Player.cs
@@ -1,16 +1,22 @@
+using System;
 using Godot;
 
 public partial class Player : CharacterBody3D {
+	[Export]
+	private int StructureDistance = 4;
+	
 	private Node3D Main;
 	private ulong StructureId = 0;
 	private bool InBuildingMode = false;
+	private Vector3 Direction;
+
 
 	public override void _Ready() {
 		Main = GetNode<Node3D>("..");
 	}
 
 	public override void _Process(double delta) {
-		if (InBuildingMode) ProjectStructureDisplay();
+		if (InBuildingMode) ProjectStructureDisplay(delta);
 	}
 
 	private void DisplayStructurePreview(PackedScene structure, bool selectionCycled) {
@@ -41,10 +47,28 @@ public partial class Player : CharacterBody3D {
 		DisplayStructurePreview(structure, true);
 	}
 
-	private void ProjectStructureDisplay() {
+	private void ProjectStructureDisplay(double delta) {
 		var s = (MeshInstance3D) InstanceFromId(StructureId);
-		s.Position = new Vector3(Position.X + 1, Position.Y + 1, Position.Z + 1);
-		GD.Print(Position);
-		//s.Transform = s.Transform.Translated(Transform.Basis.);
+		
+		var HCamRotation = GetNode<Node3D>("CameraComponent/Horizontal").GlobalTransform.Basis.GetEuler().Y;
+		var a = GetNode<Node3D>("Armature");
+		s.Rotation = new Vector3 {
+			X = s.Rotation.X,
+			Y = (float)Mathf.LerpAngle(a.Rotation.Y, HCamRotation + (float)Math.PI, delta * 7),
+			Z = s.Rotation.Z
+		};
+
+		s.Position = new Vector3 {
+			X = (float)(Math.Sin(a.Rotation.Y) * StructureDistance + Position.X),
+			Y = Position.Y + 2,
+			Z = (float)(Math.Cos(a.Rotation.Y) * StructureDistance + Position.Z)
+		};
+	}
+	
+	private void GetDirection(Vector3 direction){
+		Direction = direction.Normalized();
 	}
 }
+
+
+
