dc629eaadd934e0c05f63343aae224da7d6e3feb
Nicola Stroffolino <91049774+nicola-stroffolino@users.noreply.github.com>
2023-11-24 22:15:34 +0100

    testing some scene instancing

--- Script/Main.cs
+++ Script/Main.cs
@@ -6,12 +6,8 @@ public partial class Main : Node3D {
 	private const float UNIT = 3.5f / 2;
 	private const float DEPTH = 0.1f;
 	private const float HEIGHT = 3f / 2;
-	private MeshLibrary StructMeshLib = ResourceLoader.Load<MeshLibrary>("res://Art/structures_meshlib.tres");
+	
 
 	public override void _Ready() {
-		for (int i = 0; i < StructMeshLib.GetItemList().Length; i++) {
-			GD.Print(StructMeshLib.GetItemMesh(i));
-			//AddChild((StructMeshLib.GetItemMesh(i) as MeshInstance3D));
-		}
 	}
 }
