0cc58b190111b4d65cb180c56232565a39c3db7e
HotBaconWasTaken <91049774+nicola-stroffolino@users.noreply.github.com>
2023-12-19 09:49:37 +0100

    .

--- scripts/ui/InventoryContainer.cs
+++ scripts/ui/InventoryContainer.cs
@@ -3,13 +3,15 @@ using System;
 
 public partial class InventoryContainer : GridContainer
 {
+	[Export(PropertyHint.ResourceType, "ItemSlot")]
+	public PackedScene Slot { get; set; }
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	
+	public DisplayInventory() {
+		
 	}
 }
